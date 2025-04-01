5f9e817e8996821f9ccbc5b746a46fe62a7c1649
dany22ag <danyaguilar2003@outlook.com>
2025-04-01 10:26:58 -0600

    Update practica3.R

--- practica3.R
+++ practica3.R
@@ -1,7 +1,7 @@
 
 library(dplyr)
-clientes <- read.csv("clientes.csv")
-compras <- read.csv("compras.csv")  
+clientes <- read.csv("C:/Users/DANIELAGUADALUPEAGUI/OneDrive - TECNOLOGICO DE ESTUDIOS SUPERIORES DE IXTAPALUCA/Documentos/TESI/OCTAVO SEMESTRE/ANALISIS Y MODELADO DE DATOS/equipo/clientes.csv")
+compras <- read.csv("C:/Users/DANIELAGUADALUPEAGUI/OneDrive - TECNOLOGICO DE ESTUDIOS SUPERIORES DE IXTAPALUCA/Documentos/TESI/OCTAVO SEMESTRE/ANALISIS Y MODELADO DE DATOS/equipo/compras.csv")  
 
 fusionado <- merge(clientes, compras, by = "id_cliente", all = TRUE)
 
