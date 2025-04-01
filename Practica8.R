94d0242283bd3d9c7a985c71581edfe946d42bc3
dany22ag <danyaguilar2003@outlook.com>
2025-04-01 10:28:02 -0600

    Update Practica8.R

--- Practica8.R
+++ Practica8.R
@@ -1,7 +1,7 @@
 library(dplyr)
 
-clientes <- read.csv("clientes.csv")  # Cargar dataset de clientes
-compras <- read.csv("compras.csv")    # Cargar dataset de compras
+clientes <- read.csv("C:/Users/DANIELAGUADALUPEAGUI/OneDrive - TECNOLOGICO DE ESTUDIOS SUPERIORES DE IXTAPALUCA/Documentos/TESI/OCTAVO SEMESTRE/ANALISIS Y MODELADO DE DATOS/equipo/clientes.csv")  # Cargar dataset de clientes
+compras <- read.csv("C:/Users/DANIELAGUADALUPEAGUI/OneDrive - TECNOLOGICO DE ESTUDIOS SUPERIORES DE IXTAPALUCA/Documentos/TESI/OCTAVO SEMESTRE/ANALISIS Y MODELADO DE DATOS/equipo/compras.csv")    # Cargar dataset de compras
 
 fusionado <- left_join(clientes, compras, by = "id_cliente")
 
